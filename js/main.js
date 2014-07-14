dbb795b0b47b83226121a05ba154d0f27a870c32
snowlong <snowlong2@gmail.com>
2014-07-15 07:01:43 +0900

    画像取得リンク生成機能追加、あとレイアウトも変えた

--- js/main.js
+++ js/main.js
@@ -1,34 +1,66 @@
 $(document).ready(function() {
 
+  $('#img-link').hide();
+
   $("button:button.btn").click(function() {
+      //画像ダウンロード
+      $('#stylist-image').attr('href', 'http://www.syuhari.jp/');
+
+
       // 処理
       console.log($("#inputURL").val());
       // var html = $(#stylistInfo).load("http://beauvo.net/salon/163787/staff/388");
       var url = $("#inputURL").val();
       $.get(url, function(data){
-          //console.log(data.responseText);
+
           var responseHtml = data.responseText;
+
+          // DOMに変換
+          var doc = new DOMParser().parseFromString(data.responseText, 'application/xhtml+xml');
+          // スタイリストの画像URLを取得
+          var elm = doc.getElementById("showcase").getElementsByClassName("showcase-slide")[0];
+          var stylistImage =　elm.getElementsByClassName("showcase-content")[0].getElementsByTagName("img")[0].getAttribute("src");
+          console.log(stylistImage);
+
+          // 画像のリンクをつけて表示
+          $('#stylist-image').attr('href', stylistImage);
+
+          // スタイリストの作品画像URLを取得
+          var work_tmp = doc.getElementById("staff-work-featured").getElementsByClassName("trans")[0];
+          workImage = work_tmp.getElementsByTagName("img")[0].getAttribute("src")
+          console.log(workImage);
+
+          // 画像のリンクをつけて表示
+          $('#work-image').attr('href', workImage);
+          $('#img-link').show();
+          
+          // サロン名
           var myRe = /(<h1>)(.*)(<\/h1>)/;
           var salonName = myRe.exec(responseHtml);
           console.log(salonName[2]);
 
+          // スタイリスト氏名
           var myRe = /(<h3>)(.*)(<\/h3>)/;
           var stylistName = myRe.exec(responseHtml);
           console.log(stylistName[2]);
 
+          // スタイリスト姓
           var myRe = /^.+[\s]/;
           var stylistLastName = myRe.exec(stylistName[2]);
           console.log(stylistLastName[0]);
 
+          // スタイリストこだわり
           var myRe = /(staff-kodawari">[\s\S]*?<p>)([\s\S]*?)(<\/p>)/;
           // var myRe = /(<h4>)(.*)(<\/h4>)/;
           var stylistFeelings = myRe.exec(responseHtml);
           console.log(stylistFeelings[2]);
 
+          // 作品ページヘのパス
           var myRe = /(<div class="title">[\s\S]*?<a href=")([^"][\w\/].*?)(")/;
-           var stylistWork = myRe.exec(responseHtml);
+          var stylistWork = myRe.exec(responseHtml);
           console.log(stylistWork[2]);   
 
+          // タグ
           var tag = "＃‎beauvo‬ ‪#‎スタイリスト名鑑‬";
 
           var stylistArticle = 
